47ff4ade5168d7a4a94a74ebab02712def913607
crossfire3001 <obiwan2035@mail.ru>
2023-06-20 12:36:54 +0500

    21. Logical operators

---
+++ app.js
@@ -0,0 +1,8 @@
+// 21. Logical operators
+
+// Always [false] = 0, '', null, undefined, NaN;
+
+console.log(NaN || 2 || undefined); // 2
+console.log(NaN && 2 && undefined); // NaN
+console.log(typeof(NaN));
+console.log(NaN);
